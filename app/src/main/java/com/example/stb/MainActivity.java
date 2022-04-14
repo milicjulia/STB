b436186b1e2fff9c7fa611c8de9eab0e284c7746
Julia Milic <81866628+milicjulia@users.noreply.github.com>
2022-04-14 12:44:14 +0200

    bluetooth ne radi

--- app/src/main/java/com/example/stb/MainActivity.java
+++ app/src/main/java/com/example/stb/MainActivity.java
@@ -4,56 +4,116 @@ import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.example.stb.komunikacija.BootStart;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends Activity {
-    public static final int MESSAGE_DEVICE_NAME = 1;
-    public static final int MESSAGE_WRITE = 2;
-    public static final int MESSAGE_DISCONNECTED = 3;
-    public static final int MESSAGE_READ = 4;
     private BluetoothAdapter BA;
     private Set<BluetoothDevice> pairedDevices;
+    private BluetoothSocket BS;
     public final static int COMMUNICATION_PORT = 2000;
-
+    private static UUID uid=UUID.randomUUID();
     STBRemoteControlCommunication stbrcc;
+    private InputStream input;
+    private OutputStream output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         stbrcc = new STBRemoteControlCommunication(this);
-        Log.d("main","1");
         stbrcc.doBindService();
-        new BootStart();
-        Log.d("main","2");
+        startBluetooth();
+
+    }
+
+    public void startBluetooth(){
         BA = BluetoothAdapter.getDefaultAdapter();
+        BS = null;
         on();
         visible();
         list();
+    }
 
+    public void initSocket(BluetoothDevice device){
+        try {
+            BS = device.createRfcommSocketToServiceRecord(uid);
+        } catch (IOException e) {
+            Log.d("error", "initSocket");
+        }
+    }
+
+    public void getSocketStreams(){
+        try {
+            input = BS.getInputStream();
+            output =  BS.getOutputStream();
+        } catch (IOException e) {
+            Log.d("error", "getSocketStreams");
+        }
+    }
+
+    public void receivingData(){
+        byte[] buffer = new byte[1024];
+        int bytesCount;
+
+        while (true) {
+            try {
+                bytesCount = input.read(buffer);
+                if(buffer != null && bytesCount > 0) {
+                    Log.d("OK", "receivingData");
+                }
+            } catch (IOException e) {
+                Log.d("error", "receivingData");
+            }
+        }
+    }
+
+    public void sendingData(byte[] bytes){
+        try {
+            output.write(bytes);
+            Log.d("OK", "sendingData");
+        } catch (IOException e) {
+            Log.d("error", "sendingData");
+        }
+    }
+
+    public void connectSocket(){
+        try {
+            BS.connect();
+        } catch (IOException connEx) {
+            try {
+                BS.close();
+            } catch (IOException closeException) {
+                Log.d("error", "connectSocket");
+            }
+        }
+
+        if (BS != null && BS.isConnected()) {
+            Log.d("OK", "connectSocket");
+        }
     }
 
     public void on() {
@@ -88,8 +148,10 @@ public class MainActivity extends Activity {
                 return;
             }
         }
-        Log.d("ok", "visible");
-        startActivityForResult(getVisible, 0);
+        Log.d("OK", "visible");
+        startActivityForResult(getVisible, 1001);
+
+
     }
 
     public void list() {
@@ -101,89 +163,20 @@ public class MainActivity extends Activity {
             }
         }
         pairedDevices = BA.getBondedDevices();
-        Log.d("ok", "list");
-        int i=0;
+        int i = 0;
         for (BluetoothDevice bt : pairedDevices) {
-            Log.d("device", bt.getName());
-            if(i==0) {
+            if (i == 0) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    bt.createBond();
-                    bt.connectGatt(getApplicationContext(), true, new BluetoothGattCallback() {
-                        @Override
-                        public void onPhyUpdate(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
-                            super.onPhyUpdate(gatt, txPhy, rxPhy, status);
-                        }
-
-                        @Override
-                        public void onPhyRead(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
-                            super.onPhyRead(gatt, txPhy, rxPhy, status);
-                        }
-
-                        @Override
-                        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-                            super.onConnectionStateChange(gatt, status, newState);
-                            Log.d("conn", String.valueOf(status));
-
-                        }
-
-                        @Override
-                        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-                            super.onServicesDiscovered(gatt, status);
-                        }
-
-                        @Override
-                        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-                            super.onCharacteristicRead(gatt, characteristic, status);
-                        }
-
-                        @Override
-                        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-                            super.onCharacteristicWrite(gatt, characteristic, status);
-                        }
-
-                        @Override
-                        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-                            super.onCharacteristicChanged(gatt, characteristic);
-                        }
-
-                        @Override
-                        public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
-                            super.onDescriptorRead(gatt, descriptor, status);
-                        }
-
-                        @Override
-                        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
-                            super.onDescriptorWrite(gatt, descriptor, status);
-                        }
-
-                        @Override
-                        public void onReliableWriteCompleted(BluetoothGatt gatt, int status) {
-                            super.onReliableWriteCompleted(gatt, status);
-                        }
-
-                        @Override
-                        public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
-                            super.onReadRemoteRssi(gatt, rssi, status);
-                        }
-
-                        @Override
-                        public void onMtuChanged(BluetoothGatt gatt, int mtu, int status) {
-                            super.onMtuChanged(gatt, mtu, status);
-                        }
-
-                        @Override
-                        public void onServiceChanged(@NonNull BluetoothGatt gatt) {
-                            super.onServiceChanged(gatt);
-                        }
-                    });
+                    BluetoothDevice device = BA.getRemoteDevice(bt.getAddress());
+                    device.createBond();
+                    BA.cancelDiscovery();
+                    initSocket(device);
+                    connectSocket();
+                    getSocketStreams();
                 }
             }
             i++;
         }
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2){
-
-        }
-
     }
 
     @Override
@@ -195,12 +188,11 @@ public class MainActivity extends Activity {
     @Override
     public void onStop() {
         super.onStop();
-        this.off();
+        off();
     }
 
     @Override
     protected void onDestroy() {
-        // TODO Auto-generated method stub
         super.onDestroy();
     }
 }
